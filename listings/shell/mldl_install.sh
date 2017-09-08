a4a28061ab69e239c1ec5287ca7741c42f6aa8e6
Liam Huang <huangchencheng@360.cn>
2017-09-08 21:26:41 +0800

    [attachment] mldl_install: batch mode for Anaconda

--- listings/shell/mldl_install.sh
+++ listings/shell/mldl_install.sh
@@ -4,7 +4,7 @@ mkdir ${HOME}/mldl_downloads
 cd ${HOME}/mldl_downloads
 
 wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/archive/Anaconda2-4.4.0-Linux-x86_64.sh
-bash Anaconda2-4.4.0-Linux-x86_64.sh
+bash Anaconda2-4.4.0-Linux-x86_64.sh -b
 
 conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
 mkdir -p ~/.pip
